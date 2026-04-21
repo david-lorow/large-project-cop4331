c2ab9e02be78da60e3be046a2a877032ad99d42d
David Lorow <david.lorow@gmail.com>
2026-04-21 18:43:52 -0400

    Actual security

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,12 @@ import AboutUsPage from './pages/aboutUsPage'
 import AIReviewPage from './pages/aiReviewPage'
 import ForgotPasswordPage from './pages/forgotPasswordPage'
 import ResetPasswordPage from './pages/resetPasswordPage'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { getToken } from './api/client'
+
+function PrivateRoute({ element }: { element: JSX.Element }) {
+  return getToken() ? element : <Navigate to="/" replace />
+}
 
 function App() {
 
@@ -18,11 +23,11 @@ function App() {
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/resume/:id" element={<ResumeViewPage />} />
+      <Route path="/home" element={<PrivateRoute element={<HomePage />} />} />
+      <Route path="/resume/:id" element={<PrivateRoute element={<ResumeViewPage />} />} />
       <Route path="/verify-email" element={<VerifyEmailPage />} />
       <Route path="/about" element={<AboutUsPage />} />
-      <Route path="/ai" element={<AIReviewPage />} />
+      <Route path="/ai" element={<PrivateRoute element={<AIReviewPage />} />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/reset-password" element={<ResetPasswordPage />} />
     </Routes>
